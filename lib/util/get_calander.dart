dd4bf684dba22c2536655cc85d2ab264e52a4c4c
bininebro <bininebro@gmail.com>
2022-10-28 10:01:58 +0300

    date almost completed

--- lib/util/get_calander.dart
+++ lib/util/get_calander.dart
@@ -1,22 +1,31 @@
 import 'package:flutter_ethiopian_date_converter/mvc/model/ethiopian_date.dart';
 
-class GetCalender{
-  generateCalender(EthiopianDate date){
-    int day=getDay(month,year);
-    if(month == 13){
-      if(year+1 %4==0 ) {
+import '../mvc/model/ethiopian_day.dart';
+
+class GetCalender {
+  List<EthiopianDay> generateCalender(EthiopianDate date) {
+    int day = getDay(date.month, date.year);
+    if (date.month == 13) {
+      if (date.year + 1 % 4 == 0) {
         day = 5;
       } else {
         day = 6;
       }
     }
-    var GetTheFirstDayOftheMonth= ()
+
+    var getTheFirstDayOftheMonth = GetTheFirstDayOftheMonth(1, month, year);
+    if (date.month == 12) {
+      getDay(13, date.year);
+    }
+    if (date.month == 1) {
+      getDay(13, date.year);
+    }
   }
-  int getDay(month,year)
-  {
-    int day=30;
-    if(month == 13){
-      if(year+1 %4==0 ) {
+
+  int getDay(month, year) {
+    int day = 30;
+    if (month == 13) {
+      if (year + 1 % 4 == 0) {
         day = 5;
       } else {
         day = 6;
